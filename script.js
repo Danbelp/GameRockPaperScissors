047feb56046d781e2a3d883527a75efebc5f2896
danpebel <danpebel@gmail.com>
2022-10-28 20:21:21 -0500

    final finished game

--- script.js
+++ script.js
@@ -1,56 +1,72 @@
-let playerChoice;
-let playerSelection;
+let images = Array.from(document.querySelectorAll("[data-choice]"));
+let playerScore = document.querySelector(".playerScore");
+let computerScore = document.querySelector(".computerScore");
+let draw = document.querySelector(".draw");
+let computerChoiceImage = document.querySelector(".computerChoice")
+let playerChoiceImage = document.querySelector(".playerChoice")
+
+
+
+
+images.forEach(image => {
+    image.addEventListener("click",e => {
+    let playerSelection = image.dataset.choice;  
+    getComputerChoice() 
+    playRound(playerSelection, computerSelection)
+    console.log (computerSelection)
+    choicesImages(computerSelection,playerSelection)
+    })
+});
 
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random()*3+1)
     computerSelection = computerChoice == 1 ? "Rock": computerChoice == 2 ? "Paper" : "Scissors"
-    return console.log("The computer Choice is " + computerSelection );
+    return computerSelection;
 }
 
-function playerInput(){
-    playerSelection = prompt (playerChoice = alert("what's your choice?"))
-    userControlInput()
-    return playerChoice = console.log("You'r choice is " + playerSelection);
-}
 
-function userControlInput (){
-    if (playerSelection==("rock" || "Rock" || "ROCK")){
-        return playerSelection = "Rock"}
-    if (playerSelection==("scissors" || "Scissors" || "SCISSORS")){
-        return playerSelection = "Scissors"}
-    if (playerSelection==("paper" || "Paper" || "SCISSORS")){
-        return playerSelection = "Paper" }
-}
 
+function choicesImages(computerSelection, playerSelection){
+    if(computerSelection=="Rock"){
+        computerChoiceImage.setAttribute("src", "images/R.png")
+    } else if (computerSelection=="Scissors"){
+        computerChoiceImage.setAttribute("src", "images/S.png")
+    } else {
+        computerChoiceImage.setAttribute("src", "images/P.png")
+    }
+    if(playerSelection=="Paper"){
+        playerChoiceImage.setAttribute("src", "images/P.png")
+    } else if (playerSelection=="Scissors"){
+        playerChoiceImage.setAttribute("src", "images/S.png") 
+    } else { 
+        playerChoiceImage.setAttribute("src", "images/R.png") 
+}}
+
+
+/* 
 confirm ("Do you want to play rock paper or scissors?");
 console.info("Lets play Rock Paper Scissors");
-
-
 console.log (playerInput())
 console.log (getComputerChoice())
-
-/**
-let playerSelection = playerChoice==("rock" || "Rock" || "ROCK")
-let playerSelection = playerChoice==("scissors" || "Scissors" || "SCISSORS")
-let playerSelection = playerChoice==("paper" || "Paper" || "SCISSORS")
 */
 
-function playRound () {
+
+
+function playRound (playerSelection, computerSelection) {
     if (playerSelection=="Paper" && computerSelection=="Rock"){
-        console.log ("Human player wins!")
+        playerScore.innerText= parseInt(playerScore.innerText) + 1
     } else if(playerSelection=="Scissors" && computerSelection=="Rock"){
-        console.log ("Computer player wins!")
+        computerScore.innerText= parseInt(computerScore.innerText) + 1
     } else if (playerSelection=="Rock" && computerSelection=="Scissors"){
-        console.log ("Human player wins!")
+        playerScore.innerText= parseInt(playerScore.innerText) + 1
     } else if (playerSelection=="Paper" && computerSelection=="Scissors"){
-        console.log ("Computer player wins!")
+        computerScore.innerText= parseInt(computerScore.innerText) + 1
     } else if (playerSelection=="Scissors" && computerSelection=="Paper"){
-        console.log ("Human player wins!")
+        playerScore.innerText= parseInt(playerScore.innerText) + 1
     } else if (playerSelection=="Rock" && computerSelection=="Paper"){
-        console.log ("Computer player wins!")
+        computerScore.innerText= parseInt(computerScore.innerText) + 1
     } else {    
-        console.log ("Draw")
+        draw.innerText= parseInt(draw.innerText) + 1
     }
 } 
 
-playRound()
